29d4ac44abb5bbfbd875ece2dcfe50cb95e57f80
Alexsandro Silva <alexsandro.ssantos75@gmail.com>
2020-06-05 20:18:08 -0300

    Updates

---
+++ precache-manifest.2aec9605b0717c9d78f4ded7b67c0bb0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "335f52c39a4fffba44111d3a58f8b09b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb04d57c37286a8779a7",
+    "url": "/static/css/main.0393f47a.chunk.css"
+  },
+  {
+    "revision": "910eabc750cdde3cc341",
+    "url": "/static/js/2.5ca8d362.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/static/js/2.5ca8d362.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb04d57c37286a8779a7",
+    "url": "/static/js/main.c3c97158.chunk.js"
+  },
+  {
+    "revision": "1e184bed41a810564677",
+    "url": "/static/js/runtime-main.1ca79223.js"
+  },
+  {
+    "revision": "fe2283e1f17af7f6d2889b5692937968",
+    "url": "/static/media/home-background.fe2283e1.svg"
+  },
+  {
+    "revision": "47c4581a1dc536c1ba57fddafd6671b9",
+    "url": "/static/media/logo.47c4581a.svg"
+  }
+]);
